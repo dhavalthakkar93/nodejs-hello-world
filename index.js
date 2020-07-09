a0cedbd38118010eaf82504c92e3fb173408e9b9
Dhaval Thakkar <dhavalthakkar4995@gmail.com>
2020-07-09 19:45:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ var server = http.createServer(function(request, response) {
 var port = process.env.PORT;
 server.listen(port);
 
-console.log("Server running at http://localhost:%d", port);
+console.log("Server running on port:", port);
